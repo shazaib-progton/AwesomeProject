62299e402ac3e5acb74d9924590e6afb9ff2d9f5
shazaib-progton <shazaib@progton.com>
2022-09-14 19:17:45 +0530

    initial commit

--- src/multiply.java
+++ src/multiply.java
@@ -2,7 +2,7 @@ public class Multiply {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-
-        System.out.println("This is a sum" + (a + b));
+        int c = 3;
+        System.out.println("This is a sum" + (a + b + c));
     }
 }
