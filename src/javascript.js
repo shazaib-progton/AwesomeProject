cadc0fb8c16e4c832bf9fd38c6887326c823a149
shazaib-progton <shazaib@progton.com>
2022-09-14 16:36:28 +0530

    Added Changes

---
+++ src/javascript.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const javascript = () => {
+  return (
+    <View>
+      <Text>javascript</Text>
+    </View>
+  );
+};
+
+export default javascript;
+
+const styles = StyleSheet.create({});
---
+++ src/multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        System.out.println("This is a sum" + (a + b));
+    }
+}
